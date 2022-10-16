08a8a00367572e9f8934ef5c0828027f7228d858
agungnuraditia <112888003+agungnuraditia@users.noreply.github.com>
2022-10-17 00:39:39 +0700

    Add files via upload

---
+++ Perulangan2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int x, y;
+
+  cout << "perulangan";
+  for (x = 1; x <= 10; x++) {
+    cout << endl;
+    cout << x;
+  }
+
+  cout << "\n perulangan menurun";
+  for (y = 10; y >= 1; y--) {
+
+    cout << endl;
+    cout << y;
+  }
+}
